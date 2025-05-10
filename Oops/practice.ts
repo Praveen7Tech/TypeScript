bce37228aa88d5b483e16bc611e4550c528a4068
Pr4veenn <praveen.tech.js@gmail.com>
2025-05-10 22:38:47 +0530

    practice

--- Oops/practice.ts
+++ Oops/practice.ts
@@ -1,154 +1,154 @@
 
-Getters ans Setters
+// Getters ans Setters
 
-class Person {
-    private _name : string
-    constructor(name : string){
-        this._name = name
-    }
+// class Person {
+//     private _name : string
+//     constructor(name : string){
+//         this._name = name
+//     }
 
-    get name(): string {
-        return this._name
-    }
+//     get name(): string {
+//         return this._name
+//     }
 
-    set name(newName : string) {
-        this._name = newName
-    }
-}
+//     set name(newName : string) {
+//         this._name = newName
+//     }
+// }
 
-const person = new Person("Praveen")
+// const person = new Person("Praveen")
 
-console.log(person.name)
+// console.log(person.name)
 
-Method Overriding
+// Method Overriding
 
-class Animal {
-    speak() : void {
-        console.log("Animal speaks")
-    }
+// class Animal {
+//     speak() : void {
+//         console.log("Animal speaks")
+//     }
 
-}
+// }
 
-class Dog extends Animal {
-    speak() : void {
-        console.log("Dog bark..")
-    }
-}
+// class Dog extends Animal {
+//     speak() : void {
+//         console.log("Dog bark..")
+//     }
+// }
 
-const dog = new Dog()
-dog.speak()
-
-
-class
-
-class Person {
-    name : string;
-    age : number
-    constructor(name:string, age:number){
-        this.name = name
-        this.age = age
-    }
-
-    greet(){
-        console.log(`hai my name is ${this.name} and i am ${this.age} years old`)
-    }
-}
-
-const person = new Person("praveen", 25)
-
-person.greet()
+// const dog = new Dog()
+// dog.speak()
 
 
-Static properties and methods
+// class
 
-class Maths {
-    static num = 10
-    static multiply(value : number): number {
-        return value * this.num
-    }
-}
+// class Person {
+//     name : string;
+//     age : number
+//     constructor(name:string, age:number){
+//         this.name = name
+//         this.age = age
+//     }
 
-console.log(Maths.num)
-console.log(Maths.multiply(5))
+//     greet(){
+//         console.log(`hai my name is ${this.name} and i am ${this.age} years old`)
+//     }
+// }
 
+// const person = new Person("praveen", 25)
 
-
-Method Overloading
-
-class Operation {
-    add(a : number, b : number) : number;
-    add(a : string, b : string) : string;
-
-    add(a: any, b : any) : any {
-        return a + b
-    }
-}
-
-const operation = new Operation()
-
-console.log(operation.add(5,10))
-console.log(operation.add("hello", "world"))
+// person.greet()
 
 
-Encapsulation
+// Static properties and methods
 
-class BankAccount {
-    private balance : number = 0
+// class Maths {
+//     static num = 10
+//     static multiply(value : number): number {
+//         return value * this.num
+//     }
+// }
 
-    deposite(amount : number){
-        if(amount > 0){
-            this.balance += amount
-        }
-    }
-
-    getBalance() : number {
-        return this.balance
-    }
-}
-
-const account = new BankAccount()
-
-account.deposite(500)
-console.log(account.getBalance())
-console.log(account.balance())
+// console.log(Maths.num)
+// console.log(Maths.multiply(5))
 
 
-KeyOf 
 
-type Person = {
-    name : string;
-    age : number
-}
-type PersonKey = keyof Person; // "name" | "age"
+// Method Overloading
 
-////
-function personKey<T,K extends keyof T> (obj : T, key : K) : T[K] {
-    return obj[key]
-}
-const person = {name :"Praveen", age :25}
- const name = personKey(person, "name")
+// class Operation {
+//     add(a : number, b : number) : number;
+//     add(a : string, b : string) : string;
+
+//     add(a: any, b : any) : any {
+//         return a + b
+//     }
+// }
+
+// const operation = new Operation()
+
+// console.log(operation.add(5,10))
+// console.log(operation.add("hello", "world"))
 
 
-// MAPPED TYPES
+// Encapsulation
 
-interface Person {
-    name : string;
-    age : number
-}
+// class BankAccount {
+//     private balance : number = 0
 
-// partial
-type partialPerson = Partial<Person>
+//     deposite(amount : number){
+//         if(amount > 0){
+//             this.balance += amount
+//         }
+//     }
 
-// readonly
-type readOnlyPerson = Readonly<Person>
+//     getBalance() : number {
+//         return this.balance
+//     }
+// }
 
-// pick
-type pickName = Pick<Person, "name">
+// const account = new BankAccount()
 
-// omitt
-type personWithoutage = Omit<Person, "age">
+// account.deposite(500)
+// console.log(account.getBalance())
+// console.log(account.balance())
 
-// record
-type StringMap = Record <string, number>
 
-export {}
+// KeyOf 
+
+// type Person = {
+//     name : string;
+//     age : number
+// }
+// type PersonKey = keyof Person; // "name" | "age"
+
+// ////
+// function personKey<T,K extends keyof T> (obj : T, key : K) : T[K] {
+//     return obj[key]
+// }
+// const person = {name :"Praveen", age :25}
+//  const name = personKey(person, "name")
+
+
+// // MAPPED TYPES
+
+// interface Person {
+//     name : string;
+//     age : number
+// }
+
+// // partial
+// type partialPerson = Partial<Person>
+
+// // readonly
+// type readOnlyPerson = Readonly<Person>
+
+// // pick
+// type pickName = Pick<Person, "name">
+
+// // omitt
+// type personWithoutage = Omit<Person, "age">
+
+// // record
+// type StringMap = Record <string, number>
+
+// export {}
--- asyncronous/index.ts
+++ asyncronous/index.ts
@@ -1,216 +1,216 @@
 
 
-Promise
+// //Promise
 
-const fetchData = () : Promise <string> => {
-    return new Promise((resolve, reject) =>{
-        setTimeout(() => {
-            const success = false
-            if(success){
-                resolve ("Data fetched sucessfully")
-            }else{
-                reject ("Error fetching data")
-            }
-        }, 1000)
-    })
-}
+// const fetchData = () : Promise <string> => {
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() => {
+//             const success = false
+//             if(success){
+//                 resolve ("Data fetched sucessfully")
+//             }else{
+//                 reject ("Error fetching data")
+//             }
+//         }, 1000)
+//     })
+// }
 
-fetchData()
-.then(result => console.log(result))
-.catch(error => console.log(error))
-
-
-Promise that resolves to a string
-
-const fetchData = () : Promise<string> => {
-    return new Promise((resolve) => {
-        setTimeout(() =>{
-            resolve ("Data fetched successfully")
-        },100)
-    })
-}
-
-fetchData()
-.then(result => console.log(result))
+// fetchData()
+// .then(result => console.log(result))
+// .catch(error => console.log(error))
 
 
-Async Function
+// Promise that resolves to a string
 
-function fetchData() : Promise<string> {
-    return new Promise((resolve, reject) =>{
-        setTimeout(() =>{
-            resolve ("Data fetched succesfully compleated..")
-        }, 100)
-    })
-}
+// const fetchData = () : Promise<string> => {
+//     return new Promise((resolve) => {
+//         setTimeout(() =>{
+//             resolve ("Data fetched successfully")
+//         },100)
+//     })
+// }
 
-async function getData () {
-    const data = await fetchData()
-    console.log(data)
-}
-
-getData()
+// fetchData()
+// .then(result => console.log(result))
 
 
-// INHERITANCE 
+// Async Function
 
-class Animal {
-    name : string;
-    constructor(name : string) {
-        this.name = name
-    }
+// function fetchData() : Promise<string> {
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() =>{
+//             resolve ("Data fetched succesfully compleated..")
+//         }, 100)
+//     })
+// }
 
-    makeSound() : void {
-        console.log(`${this.name} makes sound..`)
-    }
-}
+// async function getData () {
+//     const data = await fetchData()
+//     console.log(data)
+// }
 
-class Dog extends Animal {
-    bark() : void {
-        console.log(`${this.name} bark`)
-    }
-}
-
-// const dog = new Dog("Thanos")
-
-// dog.makeSound()
-// dog.bark()
-// console.log(dog.name)
+// getData()
 
 
-// INTERFACE 
+// // INHERITANCE 
+
+// class Animal {
+//     name : string;
+//     constructor(name : string) {
+//         this.name = name
+//     }
+
+//     makeSound() : void {
+//         console.log(`${this.name} makes sound..`)
+//     }
+// }
+
+// class Dog extends Animal {
+//     bark() : void {
+//         console.log(`${this.name} bark`)
+//     }
+// }
+
+// // const dog = new Dog("Thanos")
+
+// // dog.makeSound()
+// // dog.bark()
+// // console.log(dog.name)
+
+
+// // INTERFACE 
+
+// // interface Person {
+// //     name : string;
+// //     age : number;
+// //     graduated : boolean;
+// // }
+
+// // const user : Person = {
+// //     name : "praveen",
+// //     age : 25,
+// //     graduated : true
+// // }
+
+// // CLASS 
+
+// // class Person {
+// //     name : string;
+// //     age : number;
+
+// //     constructor(name: string, age : number) {
+// //         this.name = name 
+// //         this.age = age
+// //     }
+
+// //     intro() : void {
+// //         console.log(`Hi my name is ${this.name} and i am ${this.age} years old`)
+// //     }
+// // }
+
+// // const person = new Person("praveen",25)
+
+// // person.intro()
+
+
+// // GETTERS AND SETTERS
+
+// // class Employee {
+// //     private _salary : number = 0
+
+// //     get salary() : number {
+// //         return this._salary
+// //     }
+
+// //     set salary(value : number) {
+// //         this._salary = value
+// //     }
+// // }
+
+
+// // const emp = new Employee()
+// // console.log(emp.salary)
+// // emp.salary = 10000
+// // console.log(emp.salary)
+
+
+// // TYPE ASSERTION 
+// // const input1 = <HTMLInputElement>document.getElementById("username")
+// // const input = document.getElementById("username") as HTMLInputElement
+// // console.log(input.value)
+
+// // const input2 : any = "hello world"
+// // const length = (input2 as string).length
+// // console.log(length)
+
+
+// /// API FETCH 
+// // interface Data {
+// //     id : number;
+// //     name : string
+// // }
+
+// // async function fetchData() : Promise<void> {
+// //     try {
+// //         const response = await fetch('http://sample.com')
+// //         const data : Data = await response.json()
+
+// //         console.log("Fetched data", data)
+// //     } catch (error) {
+// //         console.error("Error while fetching data..", error)
+// //     }
+// // }
+
+// // fetchData()
+
+
+// /////////////////////////////////////////////////
+
+
+// // MAPPED TYPES
 
 // interface Person {
 //     name : string;
 //     age : number;
-//     graduated : boolean;
+//     email : string;
 // }
 
-// const user : Person = {
-//     name : "praveen",
-//     age : 25,
-//     graduated : true
+// type read = Readonly<Person>
+
+// type readonly = {
+//     readonly name : string
+//     readonly age : string
 // }
 
-// CLASS 
+// type part = Partial<Person>
 
-// class Person {
-//     name : string;
-//     age : number;
-
-//     constructor(name: string, age : number) {
-//         this.name = name 
-//         this.age = age
-//     }
-
-//     intro() : void {
-//         console.log(`Hi my name is ${this.name} and i am ${this.age} years old`)
-//     }
+// type partial = {
+//     name ?: string
+//     age ?: number
+//     email ?: string
 // }
 
-// const person = new Person("praveen",25)
+// type pic = Pick<Person, "name">
 
-// person.intro()
+// type omit = Omit<Person, "age">
+
+// type record = Record<string, number>
 
 
-// GETTERS AND SETTERS
 
-// class Employee {
-//     private _salary : number = 0
+// ////////////////////////////////
 
-//     get salary() : number {
-//         return this._salary
-//     }
 
-//     set salary(value : number) {
-//         this._salary = value
+// function Area(val1 : number, val2 ?: number) : number {
+//     if(val2 === undefined){
+//         return val1 * val1
+//     }else{
+//         return val1 * val2
 //     }
 // }
 
-
-// const emp = new Employee()
-// console.log(emp.salary)
-// emp.salary = 10000
-// console.log(emp.salary)
-
-
-// TYPE ASSERTION 
-// const input1 = <HTMLInputElement>document.getElementById("username")
-// const input = document.getElementById("username") as HTMLInputElement
-// console.log(input.value)
-
-// const input2 : any = "hello world"
-// const length = (input2 as string).length
-// console.log(length)
-
-
-/// API FETCH 
-// interface Data {
-//     id : number;
-//     name : string
-// }
-
-// async function fetchData() : Promise<void> {
-//     try {
-//         const response = await fetch('http://sample.com')
-//         const data : Data = await response.json()
-
-//         console.log("Fetched data", data)
-//     } catch (error) {
-//         console.error("Error while fetching data..", error)
-//     }
-// }
-
-// fetchData()
-
-
-/////////////////////////////////////////////////
-
-
-// MAPPED TYPES
-
-interface Person {
-    name : string;
-    age : number;
-    email : string;
-}
-
-type read = Readonly<Person>
-
-type readonly = {
-    readonly name : string
-    readonly age : string
-}
-
-type part = Partial<Person>
-
-type partial = {
-    name ?: string
-    age ?: number
-    email ?: string
-}
-
-type pic = Pick<Person, "name">
-
-type omit = Omit<Person, "age">
-
-type record = Record<string, number>
-
-
-
-////////////////////////////////
-
-
-function Area(val1 : number, val2 ?: number) : number {
-    if(val2 === undefined){
-        return val1 * val1
-    }else{
-        return val1 * val2
-    }
-}
-
-console.log(Area(5))
-console.log(Area(5,4))
+// console.log(Area(5))
+// console.log(Area(5,4))
 
 
 
@@ -225,4 +225,4 @@ console.log(Area(5,4))
 
 
 
-export {}
+// export {}
